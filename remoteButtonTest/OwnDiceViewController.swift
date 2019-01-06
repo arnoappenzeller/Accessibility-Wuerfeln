cfd284bcebaca4e17507f7ab7760e485e9aeb4a9
Arno Appenzeller <arno.appenzeller@gmx.de>
2019-01-06 20:52:35 +0100

    first version with working input from headphones and a generic dice view

---
+++ remoteButtonTest/OwnDiceViewController.swift
@@ -0,0 +1,86 @@
+//
+//  OwnDiceViewController.swift
+//  Wuerfeln
+//
+//  Created by Arno Appenzeller on 06.01.19.
+//  Copyright © 2019 APPenzeller. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+
+class OwnDiceViewController: UIViewController {
+
+    @IBOutlet weak var backButton: UIButton!
+    
+    @IBOutlet weak var diceMaximumSlider: UISlider!
+    @IBOutlet weak var diceMinimumSlider: UISlider!
+    @IBOutlet weak var diceMaximumLabel: UILabel!
+    @IBOutlet weak var diceMinimumLabel: UILabel!
+    
+    @IBOutlet weak var diceResultLabel: UILabel!
+    
+    
+    var currentMinimum:Float = 1
+    var currentMax:Float = 10
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(volumeChanged(notification:)),
+                                               name: NSNotification.Name(rawValue: "AVSystemController_SystemVolumeDidChangeNotification"),
+                                               object: nil)
+        
+        let volumeView = MPVolumeView(frame: CGRect(x: -300, y: -300, width: 0, height: 0))
+        self.view.addSubview(volumeView)
+        
+        diceMinimumSlider.minimumValue = 0
+        diceMinimumSlider.maximumValue = 20
+        diceMinimumSlider.value = currentMinimum
+        
+        diceMaximumSlider.minimumValue = 0
+        diceMaximumSlider.maximumValue = 20
+        diceMaximumSlider.value = currentMax
+        
+        backButton.layer.cornerRadius = 9
+       
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func minimumSliderChanged(_ sender: Any) {
+        //get current minimum value
+        currentMinimum = (sender as! UISlider).value
+        diceMinimumLabel.text = "Minimum Augenzahl: \(Int(currentMinimum))"
+    }
+    
+    
+    @IBAction func maximumSliderChanged(_ sender: Any) {
+        //get current maximum value
+        currentMax = (sender as! UISlider).value
+        diceMaximumLabel.text = "Maximum Augenzahl: \(Int(currentMax))"
+    }
+    
+    @objc func volumeChanged(notification: NSNotification) {
+        
+        if let userInfo = notification.userInfo {
+            if let volumeChangeType = userInfo["AVSystemController_AudioVolumeChangeReasonNotificationParameter"] as? String {
+                if volumeChangeType == "ExplicitVolumeChange" {
+                    rollDice()
+                }
+            }
+        }
+    }
+    
+    func rollDice(){
+        let number = Int.random(in: Int(currentMinimum) ..< Int(currentMax+1))
+        diceResultLabel.text = "\(number)"
+        
+        
+    }
+}
--- remoteButtonTest/ViewController.swift
+++ remoteButtonTest/ViewController.swift
@@ -7,14 +7,85 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    
+    @IBOutlet weak var diceEffectView: UIVisualEffectView!
+    @IBOutlet weak var diceView: JAQDiceView!
+    @IBOutlet weak var diceResultLabel: UILabel!
+    
+    @IBOutlet weak var ownDiceButton: UIButton!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        ownDiceButton.layer.cornerRadius = 9
+        ownDiceButton.alpha = 0.7
+        
+        diceView.delegate = self
+        
+        try! AVAudioSession.sharedInstance().setActive(true, options: [])
+        try! AVAudioSession.sharedInstance().setCategory(.ambient, mode: .default, options: [])
+        NotificationCenter.default.addObserver(self, selector: #selector(volumeChanged(notification:)),
+                                               name: NSNotification.Name(rawValue: "AVSystemController_SystemVolumeDidChangeNotification"),
+                                               object: nil)
+        
+        //try AVAudioSession.sharedInstance().setActive(true)
+        
+        diceEffectView.layer.cornerRadius = 9
+        diceEffectView.layer.masksToBounds = true
+        //audioPlayer.numberOfLoops = -1
+        
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.becomeFirstResponder()
+        
+        //diceView.repositionDices()
+        
+        let frame = CGRect(x: -1000, y: -1000, width: 100, height: 100)
+        let volumeView = MPVolumeView(frame: frame)
+        volumeView.sizeToFit()
+        self.view.addSubview(volumeView)
+        //UIApplication.shared.beginReceivingRemoteControlEvents()
+        //audioPlayer.play()
+    }
+    
+    /*@IBAction func showOwnDiceView(_ sender: Any) {
+    }*/
+    
+    @objc func volumeChanged(notification: NSNotification) {
+        
+        if let userInfo = notification.userInfo {
+            if let volumeChangeType = userInfo["AVSystemController_AudioVolumeChangeReasonNotificationParameter"] as? String {
+                if volumeChangeType == "ExplicitVolumeChange" {
+                    if self.isViewLoaded && (self.view.window != nil) {
+                        // viewController is visible
+                        diceView.rollTheDice(self)
+                        print("roll the dice")
+                    }
+                }
+            }
+        }
+    }    
 
+}
 
+extension ViewController:JAQDiceProtocol{
+    func diceView(_ view: JAQDiceView!, rolledWithFirstValue firstValue: Int, secondValue: Int) {
+        diceResultLabel.text = "Würfel 1: \(firstValue)     Würfel 2: \(secondValue)"
+    }
+    
 }
 
